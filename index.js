da256f406c482d0c83a99e3c938286df496fd4eb
Salva Romero <chava0romero@Salvas-MacBook-Pro.local>
2019-05-04 18:27:58 -0600

    Tercera Entrega

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(() => $("p").hide())
 
 $(document).ready(() => {
+
     $("#button1").click(() => $("#p1").animate({
         height: 'toggle'
     }));
@@ -39,5 +40,321 @@ $(document).ready(() => {
         return false
     });
 
+    $("fixedR").click(() => {
+        $('html, body').animate({
+            scrollTop: ($('#sectionTable').offset().top)
+        }, 500);
+    })
+    // Activities 4
+
+    $("#a11").click(() => $("#p11").animate({
+        height: 'toggle'
+    }));
+    $("#a12").click(() => $("#p12").animate({
+        height: 'toggle'
+    }));
+    $("#a13").click(() => $("#p13").animate({
+        height: 'toggle'
+    }));
+    $("#a14").click(() => $("#p14").animate({
+        height: 'toggle'
+    }));
+    $("#a15").click(() => $("#p15").animate({
+        height: 'toggle'
+    }));
+    $("#a16").click(() => $("#p16").animate({
+        height: 'toggle'
+    }));
+    $("#a17").click(() => $("#p17").animate({
+        height: 'toggle'
+    }));
+    $("#a18").click(() => $("#p18").animate({
+        height: 'toggle'
+    }));
+    $("#a19").click(() => $("#p19").animate({
+        height: 'toggle'
+    }));
+    $("#a110").click(() => $("#p110").animate({
+        height: 'toggle'
+    }));
+    $("#a111").click(() => $("#p111").animate({
+        height: 'toggle'
+    }));
+    $("#a112").click(() => $("#p112").animate({
+        height: 'toggle'
+    }));
+    $("#a113").click(() => $("#p113").animate({
+        height: 'toggle'
+    }));
+    $("#a114").click(() => $("#p114").animate({
+        height: 'toggle'
+    }));
+    $("#a115").click(() => $("#p115").animate({
+        height: 'toggle'
+    }));
+    $("#a116").click(() => $("#p116").animate({
+        height: 'toggle'
+    }));
+    $("#a117").click(() => $("#p117").animate({
+        height: 'toggle'
+    }));
+    $("#a118").click(() => $("#p118").animate({
+        height: 'toggle'
+    }));
+    $("#a119").click(() => $("#p119").animate({
+        height: 'toggle'
+    }));
+
+
+
+    $("#a21").click(() => $("#p21").animate({
+        height: 'toggle'
+    }));
+    $("#a22").click(() => $("#p22").animate({
+        height: 'toggle'
+    }));
+    $("#a23").click(() => $("#p23").animate({
+        height: 'toggle'
+    }));
+    $("#a24").click(() => $("#p24").animate({
+        height: 'toggle'
+    }));
+    $("#a25").click(() => $("#p25").animate({
+        height: 'toggle'
+    }));
+    $("#a26").click(() => $("#p26").animate({
+        height: 'toggle'
+    }));
+    $("#a27").click(() => $("#p27").animate({
+        height: 'toggle'
+    }))
+    $("#a28").click(() => $("#p28").animate({
+        height: 'toggle'
+    }));
+    $("#a29").click(() => $("#p29").animate({
+        height: 'toggle'
+    }));
+    $("#a210").click(() => $("#p210").animate({
+        height: 'toggle'
+    }));
+    $("#a211").click(() => $("#p211").animate({
+        height: 'toggle'
+    }));
+
+    $("#a31").click(() => $("#p31").animate({
+        height: 'toggle'
+    }));
+    $("#a32").click(() => $("#p32").animate({
+        height: 'toggle'
+    }));
+    $("#a33").click(() => $("#p33").animate({
+        height: 'toggle'
+    }));
+    $("#a34").click(() => $("#p34").animate({
+        height: 'toggle'
+    }));
+    $("#a35").click(() => $("#p35").animate({
+        height: 'toggle'
+    }));
+
+    $("#a41").click(() => $("#p41").animate({
+        height: 'toggle'
+    }));
+    $("#a42").click(() => $("#p42").animate({
+        height: 'toggle'
+    }));
+    $("#a43").click(() => $("#p43").animate({
+        height: 'toggle'
+    }));
+    $("#a44").click(() => $("#p44").animate({
+        height: 'toggle'
+    }));
+
+    // Activities Cascada
+    $("#ac11").click(() => $("#pc11").animate({
+        height: 'toggle'
+    }));
+    $("#ac21").click(() => $("#pc21").animate({
+        height: 'toggle'
+    }));
+    $("#ac22").click(() => $("#pc22").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac23").click(() => $("#pc23").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac24").click(() => $("#pc24").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac25").click(() => $("#pc25").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac26").click(() => $("#pc26").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac27").click(() => $("#pc27").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac28").click(() => $("#pc28").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac29").click(() => $("#pc29").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac210").click(() => $("#pc210").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac211").click(() => $("#pc211").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac212").click(() => $("#pc212").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac213").click(() => $("#pc213").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac214").click(() => $("#pc214").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac215").click(() => $("#pc215").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac216").click(() => $("#pc216").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac31").click(() => $("#pc31").animate({
+        height: 'toggle'
+    }));
+    $("#ac32").click(() => $("#pc32").animate({
+        height: 'toggle'
+    }));
+    $("#ac33").click(() => $("#pc33").animate({
+        height: 'toggle'
+    }));
+    $("#ac34").click(() => $("#pc34").animate({
+        height: 'toggle'
+    }));
+    $("#ac35").click(() => $("#pc35").animate({
+        height: 'toggle'
+    }));
+    $("#ac36").click(() => $("#pc36").animate({
+        height: 'toggle'
+    }));
+    $("#ac37").click(() => $("#pc37").animate({
+        height: 'toggle'
+    }));
+    $("#ac38").click(() => $("#pc38").animate({
+        height: 'toggle'
+    }));
+    $("#ac39").click(() => $("#pc39").animate({
+        height: 'toggle'
+    }));
+    $("#ac310").click(() => $("#pc310").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac41").click(() => $("#pc41").animate({
+        height: 'toggle'
+    }));
+    $("#ac42").click(() => $("#pc42").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac43").click(() => $("#pc43").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac44").click(() => $("#pc44").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac45").click(() => $("#pc45").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac51").click(() => $("#pc51").animate({
+        height: 'toggle'
+    }));
+    $("#ac52").click(() => $("#pc52").animate({
+        height: 'toggle'
+    }));
+    $("#ac53").click(() => $("#pc53").animate({
+        height: 'toggle'
+    }));
+    $("#ac54").click(() => $("#pc54").animate({
+        height: 'toggle'
+    }));
+    $("#ac55").click(() => $("#pc55").animate({
+        height: 'toggle'
+    }));
+    $("#ac56").click(() => $("#pc56").animate({
+        height: 'toggle'
+    }));
+    $("#ac57").click(() => $("#pc57").animate({
+        height: 'toggle'
+    }));
+    $("#ac58").click(() => $("#pc58").animate({
+        height: 'toggle'
+    }));
+    $("#ac59").click(() => $("#pc59").animate({
+        height: 'toggle'
+    }));
+    $("#ac510").click(() => $("#pc510").animate({
+        height: 'toggle'
+    }));
+    $("#ac511").click(() => $("#pc511").animate({
+        height: 'toggle'
+    }));
+
+    $("#ac61").click(() => $("#pc61").animate({
+        height: 'toggle'
+    }));
+    $("#ac62").click(() => $("#pc62").animate({
+        height: 'toggle'
+    }));
+    $("#ac63").click(() => $("#pc63").animate({
+        height: 'toggle'
+    }));
+    $("#ac64").click(() => $("#pc64").animate({
+        height: 'toggle'
+    }));
+    $("#ac65").click(() => $("#pc65").animate({
+        height: 'toggle'
+    }));
+    $("#ac66").click(() => $("#pc66").animate({
+        height: 'toggle'
+    }));
+    $("#ac67").click(() => $("#pc67").animate({
+        height: 'toggle'
+    }));
+    $("#ac68").click(() => $("#pc68").animate({
+        height: 'toggle'
+    }));
+
 
 })
+
+$(document).ready(() => {
+    $("#fixedR").click(() => {
+        $("body,html").animate({
+                scrollTop: $("#sectionTable").offset().top
+            },
+            800 //speed
+        );
+    });
+});
